7cbdc481b6c1457e4bd88dbb539a48c46c9b044f
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-01-26 03:06:37 -0300

    animando background

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,17 @@ function start() {
   $("#backgroundGame").append("<div id='enemy1' class='anima2'></div>");
   $("#backgroundGame").append("<div id='enemy2' ></div>");
   $("#backgroundGame").append("<div id='friend' class='anima3'></div>");
+
+  const game = {
+    timer: setInterval(loop, 30),
+  };
+
+  function loop() {
+    moveBackground();
+  }
+
+  function moveBackground() {
+    left = parseInt($("#backgroundGame").css("background-position"));
+    $("#backgroundGame").css("background-position", left - 1);
+  }
 }
