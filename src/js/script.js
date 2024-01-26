9bb114603acad0a27b987316319a1baebce26de7
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-01-26 03:02:12 -0300

    Animando jogador, inimigo 1 e o prisioneiro!

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 function start() {
   $("#start").hide();
 
-  $("#backgroundGame").append("<div id='player'></div>");
-  $("#backgroundGame").append("<div id='enemy1'></div>");
-  $("#backgroundGame").append("<div id='enemy2'></div>");
-  $("#backgroundGame").append("<div id='friend'></div>");
+  $("#backgroundGame").append("<div id='player' class='anima1'></div>");
+  $("#backgroundGame").append("<div id='enemy1' class='anima2'></div>");
+  $("#backgroundGame").append("<div id='enemy2' ></div>");
+  $("#backgroundGame").append("<div id='friend' class='anima3'></div>");
 }
