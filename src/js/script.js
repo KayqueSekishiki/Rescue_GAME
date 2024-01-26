100a47f80b33e37349e8e3ef479ddb3d1f2294fb
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-01-26 03:17:29 -0300

    movimentando o jogador

--- src/js/script.js
+++ src/js/script.js
@@ -8,14 +8,45 @@ function start() {
 
   const game = {
     timer: setInterval(loop, 30),
+    keyPressed: [],
   };
+
+  const KEYBINDS = {
+    W: 87,
+    S: 83,
+    D: 68,
+  };
+
+  $(document).keydown(function (e) {
+    game.keyPressed[e.which] = true;
+  });
+
+  $(document).keyup(function (e) {
+    game.keyPressed[e.which] = false;
+  });
 
   function loop() {
     moveBackground();
+    movePlayer();
   }
 
   function moveBackground() {
     left = parseInt($("#backgroundGame").css("background-position"));
     $("#backgroundGame").css("background-position", left - 1);
+  }
+
+  function movePlayer() {
+    if (game.keyPressed[KEYBINDS.W]) {
+      let top = parseInt($("#player").css("top"));
+      $("#player").css("top", top - 10);
+    }
+
+    if (game.keyPressed[KEYBINDS.S]) {
+      let top = parseInt($("#player").css("top"));
+      $("#player").css("top", top + 10);
+    }
+
+    if (game.keyPressed[KEYBINDS.D]) {
+    }
   }
 }
